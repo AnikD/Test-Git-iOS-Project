71a25b6fe3d305c6c2f20362b827f25678bd0d72
Shafqat Dulal <anikdu@Shafqat-Dulals-MacBook-Pro-3.local>
2014-06-14 14:50:08 -0700

    added test class

---
+++ SDTest.h
@@ -0,0 +1,13 @@
+//
+//  SDTest.h
+//  Git Test
+//
+//  Created by Shafqat Dulal on 6/14/14.
+//  Copyright (c) 2014 SD Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDTest : NSObject
+
+@end
---
+++ SDTest.m
@@ -0,0 +1,13 @@
+//
+//  SDTest.m
+//  Git Test
+//
+//  Created by Shafqat Dulal on 6/14/14.
+//  Copyright (c) 2014 SD Tech. All rights reserved.
+//
+
+#import "SDTest.h"
+
+@implementation SDTest
+
+@end
